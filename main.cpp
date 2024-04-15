28219f4097faf9caadfce3b1b6bebf9552d1a761
mohammad-nayef <awladmohammad20@gmail.com>
2024-04-15 19:42:56 +0300

    Implemented simulator for FCFS

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <queue>
+#include <windows.h>
+
 using namespace std;
 
 struct process{
     string name;
-    int arrival_time, bursts;
+    int arrival_time, burst;
 };
 
 vector<process> processes;
 int processes_count, context_switch_time, time_quantum;
+const int SLEEPING_INTERVAL = 500;
 
 void processes_input() {
     ifstream input("..\\input.txt");
@@ -41,12 +45,12 @@ void processes_input() {
                 stream >> processes[i].arrival_time;
         }
 
-        if (line.substr(0, 11) == "CPU bursts:")
+        if (line.substr(0, 10) == "CPU burst:")
         {
-            stringstream stream(line.substr(11));
+            stringstream stream(line.substr(10));
 
             for (int i = 0; i < processes_count; i++)
-                stream >> processes[i].bursts;
+                stream >> processes[i].burst;
         }
 
         if (line.substr(0, 35) == "Context switch time (milliseconds):")
@@ -70,4 +74,41 @@ bool by_arrival_time(process p1, process p2) {
 int main() {
     processes_input();
     sort(processes.begin(), processes.end(), by_arrival_time);
+    queue<int> ready_queue;
+
+    for (int time = 0, index = 0; ; time++) {
+        if (index < processes_count && time >= processes[index].arrival_time) {
+            ready_queue.push(index);
+            index++;
+        }
+
+        if (ready_queue.empty()) {
+            if (index >= processes_count)
+                return 0;
+
+            cout << "idle ";
+            Sleep(SLEEPING_INTERVAL);
+            continue;
+        }
+
+        processes[ready_queue.front()].burst--;
+        cout << processes[ready_queue.front()].name << "-burst ";
+        
+        if (processes[ready_queue.front()].burst == 0) {
+            ready_queue.pop();
+
+            if (index == processes_count && ready_queue.empty())
+                return 0;
+
+            for (int interval = 0; interval < context_switch_time; interval++) {
+                time++;
+                cout << "switching ";
+                Sleep(SLEEPING_INTERVAL);
+            }
+
+            continue;
+        } 
+
+        Sleep(SLEEPING_INTERVAL);  
+    }
 }
